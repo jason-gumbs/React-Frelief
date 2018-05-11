702fa0395894e6370196dd7ac19bab980768a99e
Steffen Sornpao <steffenssornpao@gmail.com>
2018-05-10 23:34:23 -0400

    search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import DeleteBtn from "../components/DeleteBtn";
-import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 
 class Search extends Component {
@@ -76,33 +74,45 @@ class Search extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          
-          <Col size="md-6 sm-12" className= "m-auto">
-            <Jumbotron>
-              <h1>Resources offered List</h1>
-            </Jumbotron>
-            {this.state.Resources.length ? (
-              <List>
-                {this.state.Resources.map(resource => (
-                  <ListItem key={resource._id}>
-                    <Link to={"/resource/" + resource._id}>
-                      <strong>
-                        {resource.name} by {resource.address}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteResource(resource._id)} />
-                  </ListItem>
-                ))}
-              </List>
+      <div style={styles.container}>
+        <p style={styles.head}>Resources</p>
+          {this.state.Resources.length ? (
+            <List>
+              {this.state.Resources.map(resource => (
+                <ListItem key={resource._id}>
+                  <Link to={"/resource/" + resource._id}>
+                    <strong>
+                      {resource.name} by {resource.address}
+                    </strong>
+                  </Link>
+                  <DeleteBtn onClick={() => this.deleteResource(resource._id)} />
+                </ListItem>
+              ))}
+            </List>
             ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
-      </Container>
+          <h3>No Results to Display</h3>
+        )}
+      </div>
     );
+  }
+}
+
+const styles = {
+  container: {
+    position: "absolute",
+    top: "calc(50% - 300px)",
+    left: "calc(50% - 200px)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "400px",
+    height: "600px",
+    background: "#0D1E30"
+  },
+  head: {
+    fontSize: "50px",
+    color: "#fff"
   }
 }
 
