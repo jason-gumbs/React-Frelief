60458970853f27b01a391c52d783c2b72bf7f15b
Steffen Sornpao <steffenssornpao@gmail.com>
2018-05-10 23:24:03 -0400

    home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 import Btn from "../../components/Button";
+import logo from "./logo.png"
 
 export default class Home extends Component {
   render() {
     return (
-    <div id="content">
-      <p>FREE DISASTER RELIEF RESOURCES</p>
-      <a href="/search"><Btn>SEARCH</Btn></a>
-      <a href="/resource"><Btn>OFFER RESOURCES</Btn></a>
-      <a href="/resource/map"><Btn>Map</Btn></a>
-      <p id="copywrite">FREILIEF © 2018</p> 
-    </div>
+      <div style={styles.container}>
+        <img src={logo}/>
+        <p style={styles.head}>FRELIEF</p>
+        <a href="/search"><Btn style={styles.button}>Search</Btn></a>
+        <a href="/resource"><Btn style={styles.button}>Offer Resources</Btn></a>
+        <a href="/map"><Btn style={styles.button}>Map</Btn></a>
+      </div>
     );
+  }
+}
+
+const styles = {
+  container: {
+    position: "absolute",
+    top: "calc(50% - 300px)",
+    left: "calc(50% - 200px)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "400px",
+    height: "600px",
+    background: "#0D1E30"
+  },
+  head: {
+    fontSize: "50px",
+    color: "#fff"
+  },
+  button: {
+    padding: "5px",
+    width: "150px",
+    margin: "5px",
+    backgroundColor: "#00A3FF",
+    border: "none"
   }
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
-import Jumbotron from "../../components/Jumbotron";
-import API from "../../utils/API";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
 
 class Search extends Component {
   state = {
